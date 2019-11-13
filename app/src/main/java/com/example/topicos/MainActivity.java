2c0aa87ec32d0585d3959337829adb4932577c84
L <leidyqb12@gmail.com>
2019-11-13 12:58:46 -0500

    NoSQL funcionando

--- app/src/main/java/com/example/topicos/MainActivity.java
+++ app/src/main/java/com/example/topicos/MainActivity.java
@@ -8,9 +8,6 @@ public class MainActivity extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState){
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
-            NoSQL.calificar(3, "Más o menos", "Panchito", "Plancha");
-            NoSQL.lista_deseos("Mesa", "Panchito");
-            NoSQL.busquedas("Silla", "Panchito");
         }
 
 }
